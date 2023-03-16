a833157d5880515a9c5044f3b36051cea4bee381
Joshua Hunter <87283224+hunterjshaun@users.noreply.github.com>
2023-03-16 19:32:29 -0400

    Delete day-1-printing-start.py

--- day-1-printing-start.py
+++
@@ -1,6 +0,0 @@
-# print("Day 1 - Python Print Function")
-# print("The function is declared like this:")
-# print("print('what to print')")
-
-# Creating multiple lines
-print("Hello world!\nHello world!\nHello world!")
