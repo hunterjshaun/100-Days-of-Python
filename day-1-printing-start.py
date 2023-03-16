0d802e83ddec574f462c160a1214661084b1bffa
Josh <josh@mail.com>
2023-03-16 18:29:26 +0000

    edited day 1

---
+++ day-1-printing-start.py
@@ -0,0 +1,6 @@
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# Creating multiple lines
+print("Hello world!\nHello world!\nHello world!")
